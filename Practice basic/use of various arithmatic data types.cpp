8cf5462a6c013c63330b7d9a8a6053e13b43dfec
Nomesh-Gaur <113971266+Nomesh-Gaur@users.noreply.github.com>
2022-10-20 05:16:10 +0530

    Create use of various arithmatic data types.cpp

---
+++ Practice basic/use of various arithmatic data types.cpp
@@ -0,0 +1,42 @@
+//sum of float and integers 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a=75;
+	float b =3.34;
+	float sum = a+b;
+	cout<<"the sum of  " <<  a  << "and " << b <<"is  "
+<< sum;
+	return 0;
+}
+
+
+//multiplication float and double
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	double a=12.345268;
+	float b =3.34;
+   double c = a*b;
+	cout<<"the multiplication of  " <<  a  << "and " << b <<"is  "
+<< c;
+	return 0;
+}
+
+//divide between double and double
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	double a=12.345268;
+    double b=2.0987;
+   double c = a/b;
+	cout<<"the divide of  " <<  a  << "  and " << b <<"  is  "
+<< c;
+	return 0;
+}
