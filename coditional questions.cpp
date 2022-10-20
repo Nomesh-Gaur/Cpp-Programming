49176b7de1494ba61d0db485f1142ebf77216b60
Nomesh-Gaur <113971266+Nomesh-Gaur@users.noreply.github.com>
2022-10-20 19:24:50 +0530

    Create coditional questions.cpp

---
+++ coditional questions.cpp
@@ -0,0 +1,24 @@
+//Write a program copamre two numbers equal or not ?
+//number 1 =15,number2=15,
+//equal
+
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	cin>>a;
+	cout<< "number1 = "<< a<<endl;
+	cin >>b;
+	cout <<"number2 = "<< b<<endl;
+	
+	if(a==b){
+	cout <<"Equal"<<endl;
+	}
+	else{
+	cout<<"not Equal";
+	}
+
+	return 0;
+}
