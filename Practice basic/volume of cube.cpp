5dbfb07f0d5ca498858a95af88c9fff7e0633543
Nomesh-Gaur <113971266+Nomesh-Gaur@users.noreply.github.com>
2022-10-20 05:44:45 +0530

    Create volume of cube.cpp

---
+++ Practice basic/volume of cube.cpp
@@ -0,0 +1,13 @@
+// find the volume of a cube which side is 5
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	double a=12.345268;
+    double b=2.0987;
+   double c = a/b;
+	cout<<"the divide of  " <<  a  << "  and " << b <<"  is  "
+<< c;
+	return 0;
+}
