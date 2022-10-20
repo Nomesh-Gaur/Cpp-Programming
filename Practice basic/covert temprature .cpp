97e661fc3a16364d997fd42674e82e09b7d02854
Nomesh-Gaur <113971266+Nomesh-Gaur@users.noreply.github.com>
2022-10-20 06:10:44 +0530

    Create covert temprature .cpp

---
+++ Practice basic/covert temprature .cpp
@@ -0,0 +1,18 @@
+//fahrenheight to celcious
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,f;
+	float c;
+	f =95.0;
+    a =32;
+	cout<<"enter the temprature in fahrenheit : " << f <<endl;
+	
+	c=(f-a)*5.0/9.0;
+	cout<<"tempreture in celcious : "<< c << endl;
+	
+	
+	return 0;
+}
