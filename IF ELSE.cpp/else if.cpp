549ca8250a13bd760c4512d807946836af54af57
Nomesh-Gaur <113971266+Nomesh-Gaur@users.noreply.github.com>
2022-10-21 02:48:51 +0530

    Create else if.cpp

---
+++ IF ELSE.cpp/else if.cpp
@@ -0,0 +1,20 @@
+// write a program to three different con dtion of m to relate n
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int M;
+    cout<<"value of m : ";
+	cin>> M;
+	if(M>0){
+	  cout<<"The value of n = 1 ";
+	}
+	else if(M==0){
+	    cout<<"The value of n = 0";
+	}
+	else{
+	cout<<"the value of n = -1";
+	}
+	return 0;
+}
