aac022d11b786b67cb5a53b138893903c5807e71
Nomesh-Gaur <113971266+Nomesh-Gaur@users.noreply.github.com>
2022-10-21 02:25:56 +0530

    Create question1.cpp

---
+++ IF ELSE.cpp/question1.cpp
@@ -0,0 +1,17 @@
+//write a program to find out number is even or odd
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int A;
+	cin>>A;
+	cout<<"Number is : "<< A <<endl;
+	if(A%2==0){
+	  cout<<"Even number";
+	}
+	else{
+	cout<<"odd number ";
+	}
+	return 0;
+}
