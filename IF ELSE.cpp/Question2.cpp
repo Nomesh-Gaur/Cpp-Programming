bac7a233fa6b4a5f956bfa1ee95a694f6d6577bc
Nomesh-Gaur <113971266+Nomesh-Gaur@users.noreply.github.com>
2022-10-21 02:31:54 +0530

    Create Question2.cpp

---
+++ IF ELSE.cpp/Question2.cpp
@@ -0,0 +1,18 @@
+//write a program to check number is positive or negative 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int A;
+	cin>>A;
+	cout<<"Number is : "<< A <<endl;
+	if(A<0){
+	  cout<<"negative number ";
+	}
+	else{
+	cout<<"positive number";
+	}
+	return 0;
+}
